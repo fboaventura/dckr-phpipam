5d63bd605e83c1f945b678b30748fd557b32fe67
Frederico Freire Boaventura <frederico@boaventura.net>
2017-11-20 18:41:44 -0200

    Fixing crontab execution

--- files/cron.sh
+++ files/cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -a
 source /.mydata
+set +a
+
 /usr/bin/php /var/www/html/functions/scripts/pingCheck.php 1>/tmp/ping.log 2>&1
 /usr/bin/php /var/www/html/functions/scripts/discoveryCheck.php 1>/tmp/discovery.log 2>&1
-
 
